a2b0d33f16a31ce3b6d0392c4affcbf20889e386
SebastienBtr <sebastien29520@gmail.com>
2019-11-24 14:53:12 +0100

    change sniffer interface

--- sniffer.py
+++ sniffer.py
@@ -6,8 +6,7 @@ LISTENED_IP = os.getenv('LISTENED_IP')
 
 filter = "tcp&&(ip.dst==" + LISTENED_IP + "||ip.src==" + LISTENED_IP + ")"
 
-# TODO: get interface from env variable ?
 capture = pyshark.LiveCapture(
-    interface="en0", output_file="./capture.pcap", display_filter=filter)
+    interface="eth0", output_file="./capture.pcap", display_filter=filter)
 
 capture.sniff(timeout=SNIFFER_TIMEOUT)
