7bb1179658578113af7eba84205d0d2299d0058b
guillaumeguirriec <guirriec.guillaume@gmail.com>
2019-11-24 17:54:06 +0100

    Added INTERFACE env var

--- sniffer.py
+++ sniffer.py
@@ -3,10 +3,11 @@ import os
 
 SNIFFER_TIMEOUT = int(os.getenv('SNIFFER_TIMEOUT'))
 LISTENED_IP = os.getenv('LISTENED_IP')
+INTERFACE = os.getenv('INTERFACE')
 
 filter = "tcp&&(ip.dst==" + LISTENED_IP + "||ip.src==" + LISTENED_IP + ")"
 
 capture = pyshark.LiveCapture(
-    interface="eth0", output_file="./capture.pcap", display_filter=filter)
+    interface=INTERFACE, output_file="./capture.pcap", display_filter=filter)
 
 capture.sniff(timeout=SNIFFER_TIMEOUT)
