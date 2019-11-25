28129f9f7b39f2a5d5ff854143da309aef34489e
guillaumeguirriec <guirriec.guillaume@gmail.com>
2019-11-25 13:30:27 +0100

    Fixed filter

--- sniffer.py
+++ sniffer.py
@@ -6,7 +6,7 @@ LISTENED_IP = os.getenv('LISTENED_IP')
 INTERFACE = os.getenv('INTERFACE')
 VPN_IP = os.getenv('VPN_IP')
 
-filter = "tcp&&(ip.dst==" + VPN_IP + "||ip.src==" + LISTENED_IP + ")"
+filter = "tcp&&(ip.src!=" + VPN_IP + "&&ip.src!=" + LISTENED_IP + ")"
 
 capture = pyshark.LiveCapture(
     interface=INTERFACE, output_file="./capture.pcap", display_filter=filter)
